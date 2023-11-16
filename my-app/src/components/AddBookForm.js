590244de88541b545d4565b97b4b4777e87656ea
cristiansot <soluciongrafix@gmail.com>
2023-11-16 13:00:27 -0300

    Added fragments

--- my-app/src/components/AddBookForm.js
+++ my-app/src/components/AddBookForm.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../css/AddBookForm.css'
 
 function AddBookForm(props) {
+
+    const [input, setInput] = useState('');
+
+    const change = e => {
+        console.log('Testing...')
+    }
+
+    const sendForm = e => {
+        const newBook = {
+            id: '1',
+            title: 'Title here'
+        }
+    }
+
     return (
         <form className="contentForm">
             <input 
                 className="titleForm"
                 type="text"
                 placeholder="Write a title"
-                name="text"
+                name="title"
+                onChange={change}
             />
 
             <input 
                 className="authorForm"
                 type="text"
-                placeholder="Write a title"
-                name="text"
+                placeholder="Write a Author"
+                name="author"
             />
 
             <input 
                 className="descriptionForm"
                 type="text"
-                placeholder="Write a title"
-                name="text"
+                placeholder="Write a description"
+                name="description"
             />
             <button className="submit">
                 Add Book
--- my-app/src/components/BookList.js
+++ my-app/src/components/BookList.js
@@ -1,30 +1,46 @@
 import React, {useState} from 'react';
-import '../css/booklist.css'
+import AddBookForm from './AddBookForm';
+import Books from './Books';
+// import '../css/booklist.css'
 
-function BookList(props) {
+function BookList() {
 
-  const [] = useState();
-    return (
-      <div className='content'>
-          <img className='imgBook' src={require(`../images/book-${props.image}.jpg`)}
-          alt='Book image' />
-          <div className='contenText'>
-            <h2 className='title'>{props.title}</h2>
-            <h3 className='author'>{props.author}</h3>
-            <p className='description'>{props.description}</p>
-          </div>
+const [books, setBooks] = useState([]); 
+const addBook = book => {
+  console.log('Book added');
+  console.log('Book');
+}
+  return (
+    <>
+      <div className='list'>
+        {
+          books.map((book) => 
+            <book
+              imgBook={book.image}
+              title={book.title}
+              author={book.author}
+              description={book.description}
+           />
+          )
+        }
       </div>
-    )
+    </>
+  );
 }
 
 export default BookList;
 
-{/* <div className='content'>
-  <img className='imgBook' src={require(`../images/book-${props.image}.jpg`)}
-  alt='Book image' />
-  <div className='contenText'>
-    <h2 className='title'>{props.title}</h2>
-    <h3 className='author'>{props.author}</h3>
-    <p className='description'>{props.description}</p>
-  </div>
-</div> */}
+
+// function BookList(props) {
+//     return (
+//       <div className='content'>
+//           <img className='imgBook' src={require(`../images/book-${props.image}.jpg`)}
+//           alt='Book image' />
+//           <div className='contenText'>
+//             <h2 className='title'>{props.title}</h2>
+//             <h3 className='author'>{props.author}</h3>
+//             <p className='description'>{props.description}</p>
+//           </div>
+//       </div>
+//     )
+// }
