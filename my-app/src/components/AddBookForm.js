ccf589b1b97d9b97b22a4594099ebb702e6c1cd0
cristiansot <soluciongrafix@gmail.com>
2023-11-20 13:02:09 -0300

    Added required inputs

--- my-app/src/components/AddBookForm.js
+++ my-app/src/components/AddBookForm.js
@@ -6,7 +6,7 @@ import '../css/AddBookForm.css';
 /* The AddBookForm function is a React component that renders a form for adding a new book. */
 function AddBookForm({ addBooks }) {
   
-  const { register, handleSubmit, setValue, reset } = useForm('');
+  const { register, handleSubmit, setValue, reset, formState: {errors}} = useForm('');
   const onSubmit = handleSubmit((data) => {
 
     const newBook = {
@@ -27,39 +27,61 @@ function AddBookForm({ addBooks }) {
   /* Structure of the form */
   /* Event onSubmit for to save the data for to make a new book */
     <form className="contentForm" onSubmit={onSubmit}> 
-      <input
-        type="text"
-        placeholder="Write a title"
-        name="title"
-        {...register('title', {required: true})}
-      />
-      <input
-        type="text"
-        placeholder="Write an Author"
-        name="author"
-        {...register('author', {required: true})}
-      />
-      <textarea
-        className='descInputText'
-        type="text"
-        placeholder="Write a description"
-        name="description"
-        {...register('description', {required: true})}
-      />
-      
+      <div className='required'>
+        <input
+          type="text"
+          placeholder="Write a title"
+          name="title"
+          {...register('title', {required: true})}
+        />
+        { 
+          errors.title && <span>Title required</span>
+        }
+      </div>
+
+      <div className='required'>
+        <input
+            type="text"
+            placeholder="Write an Author"
+            name="author"
+            {...register('author', {required: true})}
+          />
+          { 
+            errors.author && <span>Author required</span>
+          }
+      </div>
+   
+      <div className='required'>
+        <textarea
+          className='descInputText'
+          type="text"
+          placeholder="Write a description"
+          name="description"
+          {...register('description', {required: true})}
+        />
+        { 
+          errors.description && <span>Description required</span>
+        }
+      </div>
+
       <button className="uploadButton">
         <i className='uploadIcon'><LuUpload /></i>
         <p>Upload File</p>
         <input
           className="image"
           type="file"
+
           // {...register('image', {required: true})}
       
           onChange={(e) => {
             // I need to resolve this point --> how to upload images at the server
 
             setValue('image', e.target.files[0].name);
-            console.log(e.target.files[0].name)
+            console.log(e.target.files[0].name);
+            
+            { 
+              errors.title && <span>Image required</span>
+            }
             
           }}
         />
